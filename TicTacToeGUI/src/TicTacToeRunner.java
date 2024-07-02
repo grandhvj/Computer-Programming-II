f7f84ad530b8b9f4b3899885399a734a717d0080
grandhvj <122465661+grandhvj@users.noreply.github.com>
2024-07-02 13:01:04 -0400

    Delete TicTacToeGUI directory

--- TicTacToeGUI/src/TicTacToeRunner.java
+++
@@ -1,9 +0,0 @@
-public class TicTacToeRunner {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            TicTacToeFrame frame = new TicTacToeFrame();
-            frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-            frame.setVisible(true);
-        });
-    }
-}
